8f1efdae9cb9876e0a75a940ce4a1cb2cd5915e9
Shivashankar-741 <shiva1894300@gmail.com>
2021-03-31 09:33:19 +0530

    refactoring backend

--- controllers/user.js
+++ controllers/user.js
@@ -1,55 +1,55 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+// import bcrypt from "bcryptjs";
+// import jwt from "jsonwebtoken";
 
-import User from "../models/user.js";
+// import User from "../models/user.js";
 
-export const signin = async (req, res) => {
-  const { email, password } = req.body;
+// export const signin = async (req, res) => {
+//   const { email, password } = req.body;
 
-  try {
-    const existingUser = await User.findOne({ email });
+//   try {
+//     const existingUser = await User.findOne({ email });
 
-    if (!existingUser) return res.status(404).json({ message: "User doesn't exist" });
+//     if (!existingUser) return res.status(404).json({ message: "User doesn't exist" });
 
-    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+//     const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
 
-    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+//     if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, "test", {
-      expiresIn: "1h",
-    });
+//     const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, "test", {
+//       expiresIn: "1h",
+//     });
 
-    res.status(200).json({ result: existingUser, token });
-  } catch (err) {
-    res.status(500).json({ message: "Something went wrong" });
-  }
-};
+//     res.status(200).json({ result: existingUser, token });
+//   } catch (err) {
+//     res.status(500).json({ message: "Something went wrong" });
+//   }
+// };
 
-export const signup = async (req, res) => {
-  const { email, password, confirmpassword, firstName, lastName } = req.body;
+// export const signup = async (req, res) => {
+//   const { email, password, confirmpassword, firstName, lastName } = req.body;
 
-  try {
-    const existingUser = await User.findOne({ email });
+//   try {
+//     const existingUser = await User.findOne({ email });
 
-    if (existingUser) return res.status(400).json({ message: "User already exists" });
+//     if (existingUser) return res.status(400).json({ message: "User already exists" });
 
-    if (password !== confirmpassword)
-      return res.status(400).json({ message: "Password don't match" });
+//     if (password !== confirmpassword)
+//       return res.status(400).json({ message: "Password don't match" });
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+//     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const result = await User.create({
-      email,
-      password: hashedPassword,
-      name: `${firstName} ${lastName}`,
-    });
+//     const result = await User.create({
+//       email,
+//       password: hashedPassword,
+//       name: `${firstName} ${lastName}`,
+//     });
 
-    const token = jwt.sign({ email: result.email, id: result._id }, "test", { expiresIn: "1h" });
+//     const token = jwt.sign({ email: result.email, id: result._id }, "test", { expiresIn: "1h" });
 
-    res.status(200).json({ result, token });
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
+//     res.status(200).json({ result, token });
+//   } catch (error) {
+//     res.status(500).json({ message: "Something went wrong" });
 
-    console.log(error);
-  }
-};
+//     console.log(error);
+//   }
+// };
--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,20 @@
 import express from "express";
 import { getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
-import auth from "../middleware/auth.js";
+// import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
+// router.get("/", getPosts);
+// router.post("/", auth, createPost);
+// router.patch("/:id", auth, updatePost);
+// router.delete("/:id", auth, deletePost);
+// router.patch("/:id/likepost", auth, likePost);
+
 router.get("/", getPosts);
-router.post("/", auth, createPost);
-router.patch("/:id", auth, updatePost);
-router.delete("/:id", auth, deletePost);
-router.patch("/:id/likepost", auth, likePost);
+router.post("/", createPost);
+// router.get("/:id", getPost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.patch("/:id/likePost", likePost);
 
 export default router;
-
